166ca001c023f3a86e8f682ff86c4059ab92f12a
toshartx <tosharevivetx@gmail.com>
2024-10-17 23:28:30 +0300

    12.3

--- Task_12.cpp
+++ Task_12.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
+const double pi = 3.141592653589;
+
+double pow(double value, int n)
+{
+    double result = 1;
+    for (int i = 0; i < n; i++)
+    {
+        result *= value;
+    }
+    return result;
+}
+
+double Factorial(int value)
+{
+    if (value == 0 || value == 1)
+    {
+        return 1;
+    } else
+    {
+        return value * Factorial(value - 1);
+    }
+}
 
 double sqrt(double value) {
     if (value < 0) {
@@ -21,38 +43,95 @@ double sqrt(double value) {
     return estimate;
 }
 
-// ax4+bx3+cx2+bx+a=0
-
-void Solve(double a, double b, double c)
-{
-    double D = b*b - 4*a*c;
-    if (D >= 0)
-    {
-        double t1 = (-b+sqrt(D))/(2*a);
-        double t2 = (-b-sqrt(D))/(2*a);
-        double D1 = t1*t1 - 4;
-        double D2 = t2*t2 - 4;
-        if (D1 >= 0)
-        {
-            std::cout << "x= " << (-t1+sqrt(D1))/2;
-            std::cout << "x= " << (-t1-sqrt(D1))/2;
-        }
-        if (D2 >= 0)
-        {
-            std::cout << "x= " << (-t2+sqrt(D1))/2;
-            std::cout << "x= " << (-t2-sqrt(D1))/2;
-        }
+double cbrt(double value) {
+    if (value == 0) {
+        return 0;
     }
-    else {
-        std::cout << "НЕТ КОРНЕЙ" << '\n';
+
+    double U = value;
+    double accuracy = 1e-7;
+    double diff = 1;
+    
+    while (std::abs(diff) > accuracy) {
+        double estimate = (2 * U + value / (U * U)) / 3;
+        diff = U - estimate;
+        U = estimate;
+    }
+
+    return U;
+}
+
+
+double cos(double value)
+{
+    double result = 0;
+    double sMember = 0;
+    for (int i = 0; i < 12; ++i)
+    {
+        sMember = (i % 2 == 0 ? 1 : -1) * pow(value, 2*i) / Factorial(2*i);
+        result += sMember;
+    }
+    return result;
+}
+
+double acos0(double value)
+{   
+    double left = 0;
+    double right = pi;
+    double xI = pi/2;
+    while (std::abs(cos(xI) - value) > 1e-9)
+    {
+        if (cos(xI) > value)
+        {
+            left = xI;
+        }
+        else
+        {
+            right = xI;
+        }
+        xI = (left+right)/2;
+    } 
+    return xI;
+}
+
+// x3+px+q=0
+
+void Solve(double p, double q)
+{
+    double D = pow(p/3.0,3)+pow(q/2.0,2);
+
+    if (D > 0)
+    {
+        double U = cbrt(-q/2+sqrt(D));
+        double V = cbrt(-q/2-sqrt(D));
+        std::cout << "x = " << U+V << '\n';
+    }
+
+    else if (D == 0)
+    {
+        double U = cbrt(q/2+sqrt(D));
+        std::cout << "x1= "<< 2*U << '\n';
+        std::cout << "x2=x3= " << -U << '\n';
+    }
+
+    else 
+    {
+        
+        double F = acos0(sqrt(-3/p)*3*q/(2*p));
+        double x1 = 2*sqrt(-p/3)*cos(F/3);
+        double x2 = 2*sqrt(-p/3)*cos(F/3 + 2*pi/3);
+        double x3 = 2*sqrt(-p/3)*cos(F/3 + 4*pi/3);
+        std::cout << "x1 = " << x1 << '\n';
+        std::cout << "x2 = " << x2 << '\n';
+        std::cout << "x3 = " << x3 << '\n';
     }
 }
 
 int main()
 {
-    double a, b, c;
-    std::cout << "Введите коэффиценты a, b и c: ";
-    std::cin >> a >> b >> c;
-    Solve(a,b,c);
+    double p, q;
+    std::cout << "Введите коэффиценты p и q: ";
+    std::cin >> p >> q;
+    Solve(p,q);
     return 0;
 }
