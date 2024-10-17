714969d4909feb78fb473fea39472c32bfcfc0e3
toshartx <tosharevivetx@gmail.com>
2024-10-17 23:26:28 +0300

    12.2

--- Task_12.cpp
+++ Task_12.cpp
@@ -21,24 +21,26 @@ double sqrt(double value) {
     return estimate;
 }
 
-//  ax4+bx2+c=0
+// ax4+bx3+cx2+bx+a=0
 
 void Solve(double a, double b, double c)
 {
-    double D1 = b*b - 4*a*c;
-    if (D1 >= 0)
+    double D = b*b - 4*a*c;
+    if (D >= 0)
     {
-        double t1 = (-b+sqrt(D1))/(2*a);
-        double t2 = (-b-sqrt(D1))/(2*a);
-        if (t1 >= 0)
+        double t1 = (-b+sqrt(D))/(2*a);
+        double t2 = (-b-sqrt(D))/(2*a);
+        double D1 = t1*t1 - 4;
+        double D2 = t2*t2 - 4;
+        if (D1 >= 0)
         {
-            std::cout << "x = " << sqrt(t1) << '\n';   
-            std::cout << "x = " << -sqrt(t1) << '\n';  
+            std::cout << "x= " << (-t1+sqrt(D1))/2;
+            std::cout << "x= " << (-t1-sqrt(D1))/2;
         }
-        if (t2 >= 0)
+        if (D2 >= 0)
         {
-            std::cout << "x = " << sqrt(t2) << '\n';   
-            std::cout << "x = " << -sqrt(t2) << '\n';  
+            std::cout << "x= " << (-t2+sqrt(D1))/2;
+            std::cout << "x= " << (-t2-sqrt(D1))/2;
         }
     }
     else {
