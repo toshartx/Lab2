7c2aa86edbc8bafa152cbed811005dc2df7d397e
toshartx <tosharevivetx@gmail.com>
2024-10-17 23:20:40 +0300

    6.5

--- Task_6.cpp
+++ Task_6.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <cmath>
-// 6.4
+// 6.5
 int main()
 {
-    double A, B, C, D, x, y, z, K;
-    std::cin >> C >> D >> x >> y >> z >> K;
-    B = sqrt(z);
-    A = x - y;
-    std::cout << cos(x) + pow(A,2)/(K - C*D) - B;
+    double A, K, x, D, n, m;
+    std::cin >> x >> K >> n >> m;
+    D = tan(x);
+    A = std::abs(n - m);
+    std::cout << 1.29 + K/A + pow(D,2);
     return 0;
 }
